32ca66211340de758e515799701a861c9077820d
Melaine Kanana <melaine.kanana@student.moringaschool.com>
2023-04-02 09:35:43 +0300

    DONE WITH ASSIGNMENT

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+function findMatching(drivers, query) {
+    return drivers.filter(driver =>
+      driver.toLowerCase() === query.toLowerCase()
+    );
+  }
+
+  function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver =>
+      driver.toLowerCase().startsWith(query.toLowerCase())
+    );
+  }
+
+  function matchName(drivers, query) {
+    return drivers.filter(driver => driver.name === query);
+  }
